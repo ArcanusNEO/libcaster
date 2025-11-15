5187424190f22d0f5863704339cdd7233dcfa2ec
ArcanusNEO <admin@transcliff.eu.org>
2025-11-15 21:41:55 +0800

    logger: Fix logger mutex name

--- src/logger.c
+++ src/logger.c
@@ -1,4 +1,4 @@
 #include "logger.h"
 
-pthread_mutex_t stdout_lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t stderr_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t logger_info_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t logger_error_lock = PTHREAD_MUTEX_INITIALIZER;
